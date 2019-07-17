a351763f263954ac4acd0168010cbb1cbc1f4526
GarciaX33 <danny.garcia80@hotmail.com>
2019-07-16 23:46:51 -0500

    will add initial files

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCCWBAidFGQirLG7PiAAX-zdCpLW53nWaM",
+    authDomain: "local-train-schedule.firebaseapp.com",
+    databaseURL: "https://local-train-schedule.firebaseio.com",
+    projectId: "local-train-schedule",
+    storageBucket: "",
+    messagingSenderId: "100022150122",
+    appId: "1:100022150122:web:b489bdd4b8ebcf66"
+  };
+  // Will Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
